f24e65fd74658a738a3878913d31134d293d0f47
ccoolawn <cornell1001@mac.com>
2016-03-09 10:15:02 -0800

    first commit

--- app/router.js
+++ app/router.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+		location: config.locationType
 });
 
 Router.map(function() {
+	this.route('needs', {}, function() {
+		this.route('physiological', {}, function() {
+			this.route('safety', {}, function() {
+				this.route('love_belonging', {}, function() {
+					this.route('esteem', {}, function() {
+						this.route('self_actualization', {});
+					});
+				});
+			});
+		});
+	});
 });
 
 export default Router;
